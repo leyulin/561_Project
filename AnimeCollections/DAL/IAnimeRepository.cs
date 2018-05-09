2d02c91a194f6f340acd288b55c1725088205550
Leyu Lin <llin@sdsu.edu>
2018-05-08 19:58:58 -0700

    Add Repository

---
+++ AnimeCollections/DAL/IAnimeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using AnimeCollections.Models;
+
+namespace AnimeCollections.DAL
+{
+    public interface IAnimeRepository : IDisposable
+    {
+        IEnumerable<Anime> GetAnime();
+        Anime GetStudentByID(int Anime);
+        void InsertAnime(Anime Anime);
+        void DeleteAnime(int AnimeID);
+        void UpdateAnime(Anime Anime);
+        void Save();
+    }
+}
